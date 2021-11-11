26dbfe9a9ef472615bf07ad429c7022e790dc2ab
bread <ismatch@qq.com>
2021-11-11 23:52:33 +0800

    单例模式

---
+++ New/Bread.DesignPattern/SingletonPattern/LoadBalancer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public class LoadBalancer
+    {
+        private static LoadBalancer _instance = null;
+
+        private IList<CustomServer> _serverList = null;
+
+        private LoadBalancer()
+        {
+            _serverList = new List<CustomServer>();
+        }
+
+        public static LoadBalancer GetLoadBalancer()
+        {
+            if (_instance == null)
+            {
+                _instance = new LoadBalancer();
+            }
+            return _instance;
+        }
+
+        public void AddServer(CustomServer server)
+        {
+            this._serverList.Add(server);
+        }
+
+        public void RemoveServer(string serverName)
+        {
+            var temp = _serverList.FirstOrDefault(x => x.Name == serverName);
+            if (temp != null)
+            {
+                this._serverList.Remove(temp);
+            }
+        }
+
+        private Random rand = new Random();
+        public CustomServer GetServer()
+        {
+            int index = rand.Next(_serverList.Count);
+
+            return _serverList[index];
+        }
+    }
+
+    public class CustomServer
+    {
+        public string Name { get; set; }
+
+        public int Size { get; set; }
+    }
+}
--- New/Bread.DesignPattern/SingletonPattern/Program.cs
+++ New/Bread.DesignPattern/SingletonPattern/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using SingletonPattern;
 
 namespace Singleton
 {
     class Program
     {
+        /*
+         * 单例（Singleton）模式：确保某一个类只有一个实例，而且自行实例化并向整个系统提供这个实例，这个类称为单例类，它提供全局访问的方法。单例模式是一种对象创建模式。
+         */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            LoadBalancer balancer, balancer2, balancer3;
+            balancer = LoadBalancer.GetLoadBalancer();
+            balancer2 = LoadBalancer.GetLoadBalancer();
+            balancer3 = LoadBalancer.GetLoadBalancer();
+
+            // 判断负载均衡器是否相同
+            if (balancer == balancer2 && balancer == balancer3 && balancer2 == balancer3)
+            {
+                Console.WriteLine("^_^ : 服务器负载均衡器是唯一的！");
+            }
+
+            // 增加服务器
+            balancer.AddServer(new CustomServer() { Name = "Server 1" });
+            balancer.AddServer(new CustomServer() { Name = "Server 2" });
+            balancer.AddServer(new CustomServer() { Name = "Server 3" });
+            balancer.AddServer(new CustomServer() { Name = "Server 4" });
+            balancer.AddServer(new CustomServer() { Name = "Server 5" });
+            balancer.AddServer(new CustomServer() { Name = "Server 6" });
+
+            // 模拟客户端请求的分发
+            for (int i = 0; i < 10; i++)
+            {
+                CustomServer server = balancer.GetServer();
+                Console.WriteLine("该请求已分配至 : " + server.Name);
+            }
 
             Console.ReadKey();
         }
