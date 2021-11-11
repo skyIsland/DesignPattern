c7b7ed6fe6bc586b3e996f3d0aadb0583800f4a7
Bread <ismatch@qq.com>
2021-11-11 13:09:00 +0800

    重新整理设计模式，快速过一遍

---
+++ New/Bread.DesignPattern/SingletonPattern/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ New/Bread.DesignPattern/SingletonPattern/Singleton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public class Singleton
+    {
+        public static Singleton _instance;
+
+        private static readonly object Locker = new object();
+
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton Instance()
+        {
+            //没有第一重 instance == null 的话，每一次有线程进入 GetInstance()时，均会执行锁定操作来实现线程同步，
+            //非常耗费性能 增加第一重instance ==null 成立时的情况下执行一次锁定以实现线程同步
+            if (_instance == null)
+            {
+                lock (Locker)
+                {
+                    //Double-Check Locking 双重检查锁定
+                    if (_instance == null)
+                    {
+                        _instance = new Singleton();
+                    }
+                }
+            }
+
+            return _instance;
+        }
+    }
+}
