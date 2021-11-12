61e69f66a8a12a9fc11c729fa370de8a71948223
bread <ismatch@qq.com>
2021-11-12 22:09:42 +0800

    简单工厂模式

---
+++ New/Bread.DesignPattern/SimpleFactory/ChartFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFactory
+{
+    public class ChartFactory
+    {
+        public static IChartable GetChart(string type)
+        {
+            IChartable chartable = null;
+
+            if (!string.IsNullOrEmpty(type))
+            {
+                if (type.Equals("histogram", StringComparison.OrdinalIgnoreCase))
+                {
+                    chartable = new HistogramChart();
+                    Console.WriteLine("初始化设置柱状图...");
+                }
+                else if (type.Equals("pie", StringComparison.OrdinalIgnoreCase))
+                {
+                    chartable = new PieChart();
+                    Console.WriteLine("初始化设置饼状图...");
+                }
+                else if (type.Equals("line", StringComparison.OrdinalIgnoreCase))
+                {
+                    chartable = new LineChart();
+                    Console.WriteLine("初始化设置折线图...");
+                }
+            }
+
+            return chartable;
+        }
+    }
+}
---
+++ New/Bread.DesignPattern/SimpleFactory/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SimpleFactory
+{
+    class Program
+    {
+        /*
+         * 简单工厂
+         * 一个工厂 依赖抽象产品
+         * 抽象产品
+         * 具体产品 实现抽象产品
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            IChartable chart = ChartFactory.GetChart("histogram");
+            if (chart != null)
+            {
+                chart.Display();
+            }
+
+            Console.WriteLine();
+
+            chart = ChartFactory.GetChart("pie");
+            if (chart != null)
+            {
+                chart.Display();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
